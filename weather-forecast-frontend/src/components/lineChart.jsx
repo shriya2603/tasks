0cd8c1120555fd83571c5a6940578cee9c8c11de
Shriya Sundriyal <sundriyalshriya@gmail.com>
2021-03-24 23:48:09 +0530

    second commit

--- weather-forecast-frontend/src/components/lineChart.jsx
+++ weather-forecast-frontend/src/components/lineChart.jsx
@@ -36,37 +36,39 @@ const newDataDisplay = {
         }
     ]
 }
-function getFeeds(){
-    newDataDisplay.labels = [];
-    newDataDisplay.datasets[0].data= [];
-    fetch("http://localhost:4000/api/getWetherData")
-            .then(result=> result.json())
-            .then(function(result){
-                newDataDisplay.labels.push("current");
-                newDataDisplay.datasets[0].data.push(result.current.temp_f);
-                const forecast = result.forecast;
-                for(let i=0;i<forecast.length;i++){
-                    const h = forecast[i];
-                    newDataDisplay.datasets[0].data.push( h.temp_f);
-                    newDataDisplay.labels.push(h.time);
-                }
-                console.log("Data from Api ");
-                console.log(newDataDisplay);
-            })
-            .catch((err)=>{
-                console.error(err);
-            });
-            console.log("Data send ");
-            console.log(newDataDisplay);
-            return newDataDisplay;
-  }
+
 
 class LineExample extends Component {
     state = { 
-        dataDisplay : getFeeds()
+        dataDisplay : newDataDisplay
      }
-
-     
+     componentDidMount(){
+        this.getFeeds();
+     }
+     getFeeds(){
+        newDataDisplay.labels = [];
+        newDataDisplay.datasets[0].data= [];
+        fetch("http://localhost:4000/api/getWetherData")
+                .then(result=> result.json())
+                .then(function(result){
+                    newDataDisplay.labels.push("current");
+                    newDataDisplay.datasets[0].data.push(result.current.temp_f);
+                    const forecast = result.forecast;
+                    for(let i=0;i<forecast.length;i++){
+                        const h = forecast[i];
+                        newDataDisplay.datasets[0].data.push( h.temp_f);
+                        newDataDisplay.labels.push(h.time);
+                    }
+                    console.log("Data from Api ");
+                    console.log(newDataDisplay);
+                    this.setState({dataDisplay: newDataDisplay});
+                })
+                .catch((err)=>{
+                    console.error(err);
+                });
+                console.log("Data send ");
+                console.log(newDataDisplay);
+      }
     render() { 
         console.log("data is "+JSON.stringify(this.state.dataDisplay));
         return (<div>
@@ -80,4 +82,3 @@ class LineExample extends Component {
 export default LineExample;
 
 
-// export default LineExample ;
